b1cc615b5b0f581e81f697366b13d9fb955232ab
Benjam-BB <benjamin.bertrand@audencia.com>
2019-11-05 21:51:54 +0100

    with helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
-    def authenticate_user!
+    def authenticate_user
         unless current_user
-          redirect_to events_path
+          flash[:alert] = "Please log in."
+          redirect_to new_user_session_path
         end
-    end
+       end
+   
+       def authenticate_current_user
+         @event = Event.find(params[:id].to_i)
+         unless current_user.id == @event.user_id
+           redirect_to events_path
+           end
+       end
 end
